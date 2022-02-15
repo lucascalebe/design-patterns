7b49f8277744b9aa7192f65a4d0ca00ac19fe7b3
Lucas Calebe <lucascalebe07@hotmail.com>
2022-02-15 14:42:51 -0300

    :zap: COM pattern Observer

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -3,18 +3,24 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class TestesPedidos {
 
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "lucas";
+        BigDecimal valorOrcamento = new BigDecimal("300");
+        int quantidadeItens = 2;
 
         GeraPedido gerador = new GeraPedido(cliente,valorOrcamento,quantidadeItens);
-        GeraPedidoHandler geraPedidoHandler = new GeraPedidoHandler(/*dependencias*/);
+        GeraPedidoHandler geraPedidoHandler = new GeraPedidoHandler(
+                Arrays.asList(new SalvarPedidoNoBancoDeDados(), new EnviarEmailPedido())
+        );
+
         geraPedidoHandler.execute(gerador);
     }
 }
--- src/br/com/alura/loja/pedido/acao/EnviarEmailPedido.java
+++ src/br/com/alura/loja/pedido/acao/EnviarEmailPedido.java
@@ -2,8 +2,9 @@ package br.com.alura.loja.pedido.acao;
 
 import br.com.alura.loja.pedido.Pedido;
 
-public class EnviarEmailPedido {
+public class EnviarEmailPedido implements AcaoAposGerarPedido {
 
+    @Override
     public void executar(Pedido pedido) {
         System.out.println("Enviando pedido por email...");
     }
