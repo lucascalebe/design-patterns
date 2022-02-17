d4d78d9960f28037f5e341ee2a5a34f710490698
Lucas Calebe <lucascalebe07@hotmail.com>
2022-02-17 16:26:34 -0300

    :poop: Compondo orçamentos SEM pattern Composite

--- src/br/com/alura/loja/TestesDescontos.java
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja;
 
 import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -8,13 +9,20 @@ import java.math.BigDecimal;
 public class TestesDescontos {
 
     public static void main(String[] args) {
-        Orcamento primeiro = new Orcamento(new BigDecimal("200"),6);
-        Orcamento segundo = new Orcamento(new BigDecimal("1000"),1);
+        Orcamento primeiro = new Orcamento();
+        primeiro.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
+
+        Orcamento segundo = new Orcamento();
+        segundo.adicionarItem(new ItemOrcamento(new BigDecimal("1000")));
+
+        Orcamento terceiro = new Orcamento();
+        segundo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 
         CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
 
         System.out.println(calculadora.calcular(primeiro));
         System.out.println(calculadora.calcular(segundo));
+        System.out.println(calculadora.calcular(terceiro));
 
     }
 }
