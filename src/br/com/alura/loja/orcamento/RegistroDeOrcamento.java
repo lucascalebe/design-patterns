2582422558304e0fbb7ed25b22ffc00fa88d257a
Lucas Calebe <lucascalebe07@hotmail.com>
2022-02-17 10:07:41 -0300

    :poop: API de registro sem pattern Adapter

---
+++ src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja.orcamento;
+
+public class RegistroDeOrcamento {
+
+    public void registrar(Orcamento orcamento) {
+        // aqui seria a chamada http para API externa
+        // não é legal porque está misturando codigo de negocio com infraestrutura
+
+    }
+}
